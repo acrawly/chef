ad2b214d56476b7ac8dcd2e3c38c9171e5021aa8
Andrew Crawford <andrew@crawford.works>
2016-03-12 17:53:58 -0700

    added in custom faxer stuff!

---
+++ faxer/README.md
@@ -0,0 +1,4 @@
+# faxer
+
+TODO: Enter the cookbook description here.
+
---
+++ faxer/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: faxer
+# Recipe:: default
+#
+# Copyright (c) 2016 Andrew Crawford, All Rights Reserved.
+
+directory '/faxer' do
+	owner 'apache'
+	group 'apache'
+	mode '0755'
+	action :create
+end
+
+directory '/faxer/apache_logs' do
+	owner 'apache'
+	group 'apache'
+	mode '0755'
+	action :create
+end
+
+file '/etc/httpd/conf.d/faxer.conf' do
+  content '
+ <VirtualHost *:80>
+  ServerAdmin support@crawford.works
+  ServerName faxer.co
+  ServerAlias www.faxer.co
+  DocumentRoot /faxer
+
+  LogLevel Warn
+  ErrorLog /faxer/apache_logs/error.log
+  CustomLog Combined /faxer/apache_logs/custom.log
+
+
+  <Directory /faxer>
+    Options +FollowSymLinks +Indexes
+    AllowOverride none 
+  </Directory>
+</VirtualHost>'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
