696c828483b0664deec688542985f6fb15ba7c89
Andrew Crawford <andrew@crawford.works>
2016-03-12 22:19:06 -0700

    changes again

--- faxer/recipes/webserver.rb
+++ faxer/recipes/webserver.rb
@@ -1,19 +1,5 @@
-app_name = 'hello world'
-app_config = node[app_name]
-app_secrets = Chef::EncryptedDataBagItem.load("secrets", app_name) 
-
 include_recipe "apache2"
 include_recipe "apache2::mod_php5" 
-
-# Set up the Apache virtual host 
-web_app app_name do 
-  server_name app_config['server_name']
-  docroot app_config['docroot']
-  #server_aliases [node['fqdn'], node['hostname']]
-  template "#{app_name}.conf.erb" 
-  log_dir node['apache']['log_dir'] 
-end
-
 
 #
 # Set up the local application config.
